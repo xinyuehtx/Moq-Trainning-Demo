306d33baed2ce67b7cb3557dd7703b9c93acc5e9
黄腾霄 <huangtengxiao@cvte.com>
2018-04-19 20:05:59 +0800

    [add]首次提交

---
+++ Moq-Trainning-Demo/Moq-Trainning-DemoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Moq-Trainning-DemoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Moq-Trainning-DemoTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ed31443-63b5-4903-b3e9-b8a3b8d9a6cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Moq-Trainning-Demo
 Moq入门系列的单元测试demo项目
+
+搭配Moq入门系列博客，味道更佳哦
+
+https://xinyuehtx.github.io/
